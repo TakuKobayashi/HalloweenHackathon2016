5a30e52405a4c4f048f80c9ac5ac273b64130ba2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-10-08 15:44:26 +0900

    TPS処理をいれた

---
+++ unity/HalloweenHackathon2016/Assets/Scripts/Common/ThirdPersonCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+	[SerializeField] Transform target;
+	[SerializeField] float distance = 5.0f;
+	[SerializeField] float height = 2.0f;
+
+	void Update () {
+		transform.position = target.position + (-Vector3.forward * distance) + (Vector3.up * height);
+		transform.LookAt(target);
+	}
+}
