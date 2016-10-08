021f8a0f19b0468af4b464eaf5fa2dfbf28ef8e2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-10-08 16:41:43 +0900

    スマホの指で動かす感じのものを作ってみた

--- unity/HalloweenHackathon2016/Assets/Scripts/Field/Character.cs
+++ unity/HalloweenHackathon2016/Assets/Scripts/Field/Character.cs
@@ -1,7 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Character : MonoBehaviour {
+	private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
+	private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+	private Vector3 m_CamForward;             // The current forward direction of the camera
+	private Vector3 m_Move;
+	private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+
+	Vector3 prevPosition = Vector3.zero;
+
 	void Start () {
+		m_Character = GetComponent<ThirdPersonCharacter>();
+		BattleController.Current.OnTouch = MoveControl;
+	}
+
+	bool MoveControl(TouchEvent touchEvent){
+		if (touchEvent.phase == TouchPhase.Began) {
+			prevPosition = touchEvent.position;
+		} else if (touchEvent.phase == TouchPhase.Moved) {
+			Vector3 movedPosition = touchEvent.position - prevPosition;
+			move (movedPosition);
+//			this.transform.position = transform.position + movedPosition;
+			prevPosition = touchEvent.position;
+		} else {
+			move (Vector3.zero);
+		}
+		return true;
+	}
+
+	private void move(Vector3 movePosition){
+		Vector3 move = movePosition.y * Vector3.forward + movePosition.x * Vector3.right;
+		m_Character.Move(move, false, false);
 	}
 }
