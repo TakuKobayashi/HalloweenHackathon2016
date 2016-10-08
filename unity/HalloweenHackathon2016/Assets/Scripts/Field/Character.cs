97134af336c4b7da28181807e797dc96bb6264ee
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-10-08 17:48:19 +0900

    操作感がなんか違ったのでちゃんと動くように修正しました。

--- unity/HalloweenHackathon2016/Assets/Scripts/Field/Character.cs
+++ unity/HalloweenHackathon2016/Assets/Scripts/Field/Character.cs
@@ -3,35 +3,34 @@ using System.Collections;
 using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Character : MonoBehaviour {
-	private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-	private Transform m_Cam;                  // A reference to the main camera in the scenes transform
-	private Vector3 m_CamForward;             // The current forward direction of the camera
-	private Vector3 m_Move;
-	private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
-
-	Vector3 prevPosition = Vector3.zero;
+	private ThirdPersonCharacter TPSCharacter;
+	Vector3 startPosition = Vector3.zero;
+	Vector3 movePosition = Vector3.zero;
 
 	void Start () {
-		m_Character = GetComponent<ThirdPersonCharacter>();
+		TPSCharacter = GetComponent<ThirdPersonCharacter>();
 		BattleController.Current.OnTouch = MoveControl;
+	}
+
+	void Update(){
+		move(movePosition);
 	}
 
 	bool MoveControl(TouchEvent touchEvent){
 		if (touchEvent.phase == TouchPhase.Began) {
-			prevPosition = touchEvent.position;
+			movePosition = Vector3.zero;
+			startPosition = touchEvent.position;
 		} else if (touchEvent.phase == TouchPhase.Moved) {
-			Vector3 movedPosition = touchEvent.position - prevPosition;
-			move (movedPosition);
-//			this.transform.position = transform.position + movedPosition;
-			prevPosition = touchEvent.position;
+			movePosition = touchEvent.position - startPosition;
 		} else {
-			move (Vector3.zero);
+			startPosition = Vector3.zero;
+			movePosition = Vector3.zero;
 		}
 		return true;
 	}
 
 	private void move(Vector3 movePosition){
 		Vector3 move = movePosition.y * Vector3.forward + movePosition.x * Vector3.right;
-		m_Character.Move(move, false, false);
+		TPSCharacter.Move(move, false, false);
 	}
 }
